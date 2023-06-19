8ba85a07531b5ba145c79693b146f8c048b46287
Remy Derollez <remy@loftorbital.com>
2023-06-19 12:05:01 +0000

    style: apply style in python

--- bindings/python/test/test_import.py
+++ bindings/python/test/test_import.py
@@ -1,7 +1,6 @@
 # Apache License 2.0
 
+
 class TestImport:
-
-    def test_import (self):
-
+    def test_import(self):
         from ostk.simulation import Satellite
--- bindings/python/test/test_satellite.py
+++ bindings/python/test/test_satellite.py
@@ -16,25 +16,23 @@ from ostk.simulation import SatelliteConfiguration
 
 
 class TestSatellite:
-
-    def test_configure (self):
-
+    def test_configure(self):
         environment: Environment = Environment.default()
 
         orbit = Orbit.sun_synchronous(
-            epoch = Instant.date_time(datetime(2020, 1, 1, 0, 0, 0), Scale.UTC),
-            altitude = Length.kilometers(500.0),
-            local_time_at_descending_node = Time(14, 0, 0),
-            celestial_object = environment.access_celestial_object_with_name('Earth'),
+            epoch=Instant.date_time(datetime(2020, 1, 1, 0, 0, 0), Scale.UTC),
+            altitude=Length.kilometers(500.0),
+            local_time_at_descending_node=Time(14, 0, 0),
+            celestial_object=environment.access_celestial_object_with_name("Earth"),
         )
 
         satellite = Satellite.configure(
-            configuration = SatelliteConfiguration(
-                id = '9ea22c07-6977-48a7-8f68-dff758971d57',
-                name = 'LoftSat-1',
-                profile = Profile.nadir_pointing(
-                    orbit = orbit,
-                    orbital_frame_type = Orbit.FrameType.VVLH,
+            configuration=SatelliteConfiguration(
+                id="9ea22c07-6977-48a7-8f68-dff758971d57",
+                name="LoftSat-1",
+                profile=Profile.nadir_pointing(
+                    orbit=orbit,
+                    orbital_frame_type=Orbit.FrameType.VVLH,
                 ),
             ),
         )
