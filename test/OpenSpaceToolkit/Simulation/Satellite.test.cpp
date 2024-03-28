dc8f0f5d7f5ea67ad8bdd0c44fa3f5b88195389e
Antoine Paletta <98616558+apaletta3@users.noreply.github.com>
2024-03-28 15:46:17 +0100

    feat: add-access profile (#27)
    * wip: memory leak in python test
    
    * chore: update to OSTk astro 9.0.3
    
    ---------
    
    Co-authored-by: Vishwa Shah <vishwa@loftorbital.com>
    Co-authored-by: Vishwa Shah <vishwa2710@gmail.com>

--- test/OpenSpaceToolkit/Simulation/Satellite.test.cpp
+++ test/OpenSpaceToolkit/Simulation/Satellite.test.cpp
@@ -5,50 +5,65 @@
 #include <OpenSpaceToolkit/Simulation/Component/State.hpp>
 #include <OpenSpaceToolkit/Simulation/Satellite.hpp>
 
-#include <OpenSpaceToolkit/Mathematics/Geometry/3D/Object/Point.hpp>
-#include <OpenSpaceToolkit/Mathematics/Geometry/3D/Object/Polygon.hpp>
-#include <OpenSpaceToolkit/Mathematics/Geometry/3D/Object/Pyramid.hpp>
-#include <OpenSpaceToolkit/Mathematics/Geometry/3D/Transformation/Rotation/RotationMatrix.hpp>
+#include <OpenSpaceToolkit/Physics/Environment/Object/Celestial/Earth.hpp>
+#include <OpenSpaceToolkit/Physics/Time/Instant.hpp>
+#include <OpenSpaceToolkit/Physics/Time/Time.hpp>
+#include <OpenSpaceToolkit/Physics/Unit/Length.hpp>
 
 #include <OpenSpaceToolkit/Astrodynamics/Flight/Profile.hpp>
 
 #include <Global.test.hpp>
 
+using ostk::core::type::Shared;
+using ostk::core::type::String;
+using ostk::core::container::Array;
+
+using ostk::physics::unit::Length;
+using ostk::physics::time::Time;
+using ostk::physics::time::Instant;
+using ostk::physics::environment::object::celestial::Earth;
+
+using ostk::astrodynamics::flight::Profile;
+using ostk::astrodynamics::trajectory::Orbit;
+
+using ostk::simulation::Component;
+using ostk::simulation::ComponentConfiguration;
+using ostk::simulation::component::Geometry;
+using ostk::simulation::component::GeometryConfiguration;
 using ostk::simulation::Satellite;
+using ostk::simulation::SatelliteConfiguration;
 
 class OpenSpaceToolkit_Simulation_Satellite : public ::testing::Test
 {
    protected:
     void SetUp() override
     {
-        using ostk::core::type::Shared;
-        using ostk::core::type::String;
-        using ostk::core::container::Array;
+        const Orbit orbit = Orbit::SunSynchronous(
+            Instant::J2000(), Length::Kilometers(7000.0), Time::Noon(), std::make_shared<Earth>(Earth::Default())
+        );
 
-        using ostk::astrodynamics::flight::Profile;
+        profile_ = Profile::NadirPointing(orbit, Orbit::FrameType::VVLH);
 
-        using ostk::simulation::Component;
-        using ostk::simulation::component::Geometry;
-
-        this->satellite = Satellite(
+        this->satellite_ = Satellite(
             "87da0b5f-9f65-4c5c-a660-bd254742960b",
             "LoftSat-1",
             {"a", "b"},
             Array<Shared<Geometry>>::Empty(),
             Array<Shared<Component>>::Empty(),
             nullptr,
-            std::make_shared<Profile>(Profile::Undefined()),
+            std::make_shared<Profile>(profile_),
             nullptr
         );
     }
 
-    Satellite satellite = Satellite::Undefined();
+    Profile profile_ = Profile::Undefined();
+    Satellite satellite_ = Satellite::Undefined();
 };
 
 TEST_F(OpenSpaceToolkit_Simulation_Satellite, IsDefined)
 {
     {
-        EXPECT_TRUE(satellite.isDefined());
+        EXPECT_TRUE(satellite_.isDefined());
     }
 
     {
@@ -59,7 +74,7 @@ TEST_F(OpenSpaceToolkit_Simulation_Satellite, IsDefined)
 TEST_F(OpenSpaceToolkit_Simulation_Satellite, getId)
 {
     {
-        EXPECT_EQ(satellite.getId(), "87da0b5f-9f65-4c5c-a660-bd254742960b");
+        EXPECT_EQ(satellite_.getId(), "87da0b5f-9f65-4c5c-a660-bd254742960b");
     }
 
     {
@@ -70,7 +85,7 @@ TEST_F(OpenSpaceToolkit_Simulation_Satellite, getId)
 TEST_F(OpenSpaceToolkit_Simulation_Satellite, getName)
 {
     {
-        EXPECT_EQ(satellite.getName(), "LoftSat-1");
+        EXPECT_EQ(satellite_.getName(), "LoftSat-1");
     }
 
     {
@@ -85,5 +100,31 @@ TEST_F(OpenSpaceToolkit_Simulation_Satellite, Undefined)
     }
 }
 
-// TEST_F (OpenSpaceToolkit_Simulation_Satellite, Configure)
+TEST_F(OpenSpaceToolkit_Simulation_Satellite, Configure)
+{
+    const SatelliteConfiguration satelliteConfiguration = {
+        "87da0b5f-9f65-4c5c-a660-bd254742960b",
+        "LoftSat-1",
+        profile_,
+        Array<ComponentConfiguration>::Empty(),
+        {"a", "b"},
+        Array<GeometryConfiguration>::Empty(),
+    };
+
+    const Shared<Satellite> satellite = Satellite::Configure(satelliteConfiguration, nullptr);
+
+    EXPECT_TRUE(satellite->isDefined());
+}
+
+TEST_F(OpenSpaceToolkit_Simulation_Satellite, AccessProfile)
+{
+    {
+        EXPECT_NO_THROW(satellite_.accessProfile()->isDefined());
+    }
+
+    {
+        EXPECT_ANY_THROW(Satellite::Undefined().accessProfile());
+    }
+}
+
 // TEST_F (OpenSpaceToolkit_Simulation_Satellite, GenerateFrame)
