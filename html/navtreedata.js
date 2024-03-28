f207aba6db5301c06aa442c53e547ab0db204ada
apaletta3 <apaletta3@users.noreply.github.com>
2024-03-28 09:17:22 +0000

    Deploying to gh-pages from @ open-space-collective/open-space-toolkit-simulation@147c264c13020962b5259b418d532776421d4334 🚀

--- html/navtreedata.js
+++ html/navtreedata.js
@@ -34,22 +34,21 @@ var NAVTREE =
       ] ],
       [ "Installation", "index.html#autotoc_md6", [
         [ "C++", "index.html#autotoc_md7", [
-          [ "Debian / Ubuntu", "index.html#autotoc_md8", null ],
-          [ "Fedora / CentOS", "index.html#autotoc_md9", null ]
+          [ "Debian / Ubuntu", "index.html#autotoc_md8", null ]
         ] ],
-        [ "Python", "index.html#autotoc_md10", null ]
+        [ "Python", "index.html#autotoc_md9", null ]
       ] ],
-      [ "Documentation", "index.html#autotoc_md11", null ],
-      [ "Tutorials", "index.html#autotoc_md12", null ],
-      [ "Setup", "index.html#autotoc_md13", [
-        [ "Development Environment", "index.html#autotoc_md14", null ],
-        [ "Build", "index.html#autotoc_md15", null ],
-        [ "Test", "index.html#autotoc_md16", null ]
+      [ "Documentation", "index.html#autotoc_md10", null ],
+      [ "Tutorials", "index.html#autotoc_md11", null ],
+      [ "Setup", "index.html#autotoc_md12", [
+        [ "Development Environment", "index.html#autotoc_md13", null ],
+        [ "Build", "index.html#autotoc_md14", null ],
+        [ "Test", "index.html#autotoc_md15", null ]
       ] ],
-      [ "Dependencies", "index.html#autotoc_md17", null ],
-      [ "Contribution", "index.html#autotoc_md18", null ],
-      [ "Special Thanks", "index.html#autotoc_md19", null ],
-      [ "License", "index.html#autotoc_md20", null ]
+      [ "Dependencies", "index.html#autotoc_md16", null ],
+      [ "Contribution", "index.html#autotoc_md17", null ],
+      [ "Special Thanks", "index.html#autotoc_md18", null ],
+      [ "License", "index.html#autotoc_md19", null ]
     ] ],
     [ "Contributing", "md__c_o_n_t_r_i_b_u_t_i_n_g.html", [
       [ "Introduction", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#Introduction", null ],
